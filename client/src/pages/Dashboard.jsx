5e8710ff3b6d6919c41a10d20cf0ebde1cc7ee9e
Luis00809 <davidcarvajal008@gmail.com>
2023-12-08 10:08:35 -0800

    sets up react pages and works

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+export default function DashBoard () {
+    return (
+        <div className="container">
+            <h3>User Dashboard section</h3>
+        </div>
+    )
+}
---
+++ client/src/pages/RoomListings.jsx
@@ -0,0 +1,8 @@
+export default function RoomListings () {
+    return (
+        <div>
+            <h3>room posting page</h3>
+            <p>create card component</p>
+        </div>
+    )
+}
